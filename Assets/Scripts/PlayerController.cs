f726320b6e7f33f6c3450beccb22575669b1991a
PremiumPasta <123001496+PremiumPasta@users.noreply.github.com>
2023-01-25 19:03:22 -0500

    Final copy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,20 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
     }
-
-
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
@@ -24,10 +31,31 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
-    void fixedupdate();
+    void FixedUpdate()
     {
-    Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
-    rb.AddForce(movement);
+        rb.AddForce(movement * speed);
     }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if( count >= 14)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Point"))
+        { 
+            other.gameObject.SetActive(false); 
+            count = count + 1;
+
+            SetCountText();
+        } 
+        
+    }
+    
 }
